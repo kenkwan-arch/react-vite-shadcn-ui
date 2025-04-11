a93f4ed757b7101e39a853778f9b6478f4c83ae0
kenkwan-arch <ken.kwan@arch-partnership.com>
2025-04-11 04:08:19 +0000

    chora: init

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8002,
+    host: "127.0.0.1",
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
